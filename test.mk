b47af69dd845a3c1207da53535d1adafbb83392f
robbbalmbra <robbalmbra@gmail.com>
2020-05-07 17:57:59 +0100

    test

---
+++ test.mk
@@ -0,0 +1,25 @@
+dfgdf
+g
+df
+gh
+fg
+hd
+fgh
+d
+f
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    lineage.livedisplay@2.0-service.samsung-exynos
+
+dfghdfgh
+d
+fgh
+d
+fg
+hdfghdfghdfghdfdghdfmuiltfgdfghsd
+fgsfgh
+d
+j
+d
+
